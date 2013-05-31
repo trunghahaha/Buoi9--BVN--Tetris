3e6ee7f60b49ec9d59e644ba953e57fafc394e9d
do duc trung <trung_xi_ke_le_be@yahoo.com>
2013-06-01 02:02:26 +0700

    Initial Commit

---
+++ Buoi9- BVN- Tetris/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Buoi9- BVN- Tetris
+//
+//  Created by doductrung on 6/1/13.
+//  Copyright (c) 2013 doductrung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Buoi9- BVN- Tetris/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Buoi9- BVN- Tetris
+//
+//  Created by doductrung on 6/1/13.
+//  Copyright (c) 2013 doductrung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
